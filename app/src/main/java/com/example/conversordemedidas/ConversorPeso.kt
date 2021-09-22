d943d2b9bfd3aeac7c6a9d860c52b5756f3fea84
Roberto arenhart <rbrtarenhart@gmail.com>
2021-09-22 20:15:00 -0300

    Criando um ArrayAdapter para listar as unidades de medida no componente Spinner

--- app/src/main/java/com/example/conversordemedidas/ConversorPeso.kt
+++ app/src/main/java/com/example/conversordemedidas/ConversorPeso.kt
@@ -3,11 +3,22 @@ package com.example.conversordemedidas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class ConversorPeso : AppCompatActivity() {
+
+    var unidades = arrayOf("Miligrama (mg)", "Centigrama (cg)", "Grama (g)", "Quilograma (kg)")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_conversor_peso)
+
+        //Carregar os componentes
+        val spinner = findViewById<Spinner>(R.id.selectPeso)
+
+        //Cria Array Adapter
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, unidades)
+        spinner.adapter = adapter
 
         //Ativar o "up navigation" na ActionBar
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
--- app/src/main/java/com/example/conversordemedidas/ConversorTemperatura.kt
+++ app/src/main/java/com/example/conversordemedidas/ConversorTemperatura.kt
@@ -3,11 +3,22 @@ package com.example.conversordemedidas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class ConversorTemperatura : AppCompatActivity() {
+
+    var unidades = arrayOf("Celcius (c)", "Kelvin (K)", "Fahrennheit (F)")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_conversor_temperatura)
+
+        //Carregar os componentes
+        val spinner = findViewById<Spinner>(R.id.selectTemperatura)
+
+        //Cria Array Adapter
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, unidades)
+        spinner.adapter = adapter
 
         //Ativar o "up navigation" na ActionBar
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
