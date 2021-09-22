288437d58d8cd13de417e07fe35419a41919a52f
Roberto arenhart <rbrtarenhart@gmail.com>
2021-09-22 17:35:37 -0300

    first commit - visual pronto e primeira navegacao

---
+++ app/src/main/java/com/example/conversordemedidas/ConversorTemperatura.kt
@@ -0,0 +1,11 @@
+package com.example.conversordemedidas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConversorTemperatura : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_conversor_temperatura)
+    }
+}
